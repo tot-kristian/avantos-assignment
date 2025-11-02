581d12680f51bb23d6537b52e4de0a00d39b5191
Kristian Tot <kristiantot23@gmail.com>
2025-11-02 08:47:48 +0100

    add env variable for api base url

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-const baseUrl = "http://localhost:3000/api/v1"; // TODO add base url
+const baseUrl = import.meta.env.VITE_AVANTOS_BASE_URL;
 
 export const apiClient = axios.create({ baseURL: baseUrl });
---
+++ vite-env.d.ts
@@ -0,0 +1,7 @@
+interface ImportMetaEnv {
+  readonly VITE_AVANTOS_BASE_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
