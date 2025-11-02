103efe8fafb20302ff7b04b666b8ffce5a6530ef
Kristian Tot <kristiantot23@gmail.com>
2025-11-02 23:47:12 +0100

    Add comment instead of resolving TODO

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,10 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <Layout>
-        {/*// TODO extract these to mock data as these woukd be query params in*/}
-        {/*real world*/}
+        {/* NOTE: In a real-world scenario, `tenantId` and `blueprintId` would be
+          passed as query parameters or derived dynamically (e.g., from the route or user context).
+          Since the API always returns the same static data for this assignment,
+          these values are hardcoded here for simplicity and clarity. */}
         <GraphProvider tenantId="1" blueprintId="2">
           <Graph />
         </GraphProvider>
